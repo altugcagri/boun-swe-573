02b1af24f94e9c8cfdbfa5eddb9c9edd11be3afa
Cagri Altug <cagri.altug@siemens.com>
2019-05-15 16:11:28 +0300

    web deploymnet

--- web/src/constants/index.js
+++ web/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://18.195.215.49:8080/api';
+export const API_BASE_URL = 'http://18.184.25.154:8080/api';
 // export const API_BASE_URL = 'http://192.168.1.55:8080/api';
 // export const API_BASE_URL = '/api';
 export const ACCESS_TOKEN = 'accessToken';
